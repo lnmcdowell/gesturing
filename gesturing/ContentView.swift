db5f5e414dfa3bbf04f3ddc8003cc8d087c3b4e9
Larry Nathaniel Mcdowell <nathaniel29651@gmail.com>
2021-02-21 21:36:54 -0500

    Initial Commit

---
+++ gesturing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  gesturing
+//
+//  Created by Larry Mcdowell on 2/21/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ gesturing/gesturingApp.swift
@@ -0,0 +1,17 @@
+//
+//  gesturingApp.swift
+//  gesturing
+//
+//  Created by Larry Mcdowell on 2/21/21.
+//
+
+import SwiftUI
+
+@main
+struct gesturingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
